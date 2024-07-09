7571628a6bc2d0dabcdb0f9b94fa0c8b63f8a99d
math.arantes <matheusoficial1411@gmail.com>
2024-07-09 18:00:16 -0300

    curso 5%

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        //ALT + 1 abre ou  fecha a aba a esquerda da tela do intellij
+        System.out.println("Hello world!!!");
     }
 }
