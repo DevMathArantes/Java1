8115a53b4cb9483c3697beea0142903d3d0e6b67
math.arantes <matheusoficial1411@gmail.com>
2024-07-10 14:08:37 -0300

    curso 28%

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,6 +1,85 @@
 public class Main {
     public static void main(String[] args) {
-        //ALT + 1 abre ou  fecha a aba a esquerda da tela do intellij
+        /*
+        *ALT + 1 abre ou  fecha a aba a esquerda da tela do intellij
+
+        *Link com dicas da alura sobre o intellij
+        *https://www.alura.com.br/artigos/intellij-idea-dicas-truques-usar-no-dia-a-dia
+
+        *Link sobre a execução
+        *https://www.alura.com.br/artigos/jvm-conhecendo-processo-execucao-de-codigo
+
+        *Boas práticas:
+        *   MinhaClasse
+        *   meuMetodo
+        *   MINHA_CONSTANTE
+        *   minhaVariavel
+
+        *As diferenças entre JVM, JRE e JDK:
+
+            * JVM (Java Virtual Machine) - é a Máquina Virtual do Java responsável por
+            *  executar o bytecode;
+
+            * JRE (Java Runtime Environment) - Ambiente de Execução do Java que fornece
+            * as bibliotecas padrões do Java para o JDK compilar o seu código e para a
+            * JVM executar o seu programa;
+
+            * JDK (Java Development Kit) - Kit de Desenvolvimento Java responsável por
+            * compilar código-fonte em bytecode.
+
+        *   Escrever ... na tela (pode ser valores ou conteúdo dentro de " ", separados por ","
+        *   System.out.println(...);
+
+        *Declaração de variáveis
+        * TIPO NOME = VALOR;
+        * ou
+        * TIPO NOME;
+        * NOME = VALOR;
+        *
+        *   Tipos:
+        *   int : valores inteiros de -2147483648 a 2147483647
+        *   byte : valores inteiros de -128 a 127
+        *   boolean: true or false (verdadeiro ou falso)
+        *   char : 1 caractere
+        *   short : valores inteiros de -32768 a 32767
+        *   long : valores inteiros -9223372036854775808 a 9223372036854775807
+        *   float : valores decimais com até 7 digitos
+        *   double : valores decimais com até 15 digitos
+
+        *Operadores:
+
+        *   Atribuição
+        *   valor1 = valor2 : atribui valor2 a valor1
+        *   valor1 += valor2 : valor1 será valor1 + valor2
+
+        *   Aritméticos
+        *   + : adição
+        *   - : subtração
+        *   * : multiplicação
+        *   / : divisão
+        *   % : resto de divisão
+
+        *   Comparação
+        *   x == y : x igual y
+        *   x != y : x diferente de y
+        *   x > y : x maior que y
+        *   x >= y : x maior ou igual a y
+        *   x < y : x menor que y
+        *   x <= y : x menor ou igual a y
+
+        *   Lógicos
+        *   && : and
+        *   || : or
+        *   ! : not
+
+        *   Incremento
+        *   ++x : x = x + 1
+
+        */
         System.out.println("Hello world!!!");
+        System.out.println("Filme: A rede social");
+        int anoLancamento = 2022;
+        System.out.println("Ano de lançamento: "+anoLancamento);
+        boolean incluidoNoPlano = true ;
     }
 }
